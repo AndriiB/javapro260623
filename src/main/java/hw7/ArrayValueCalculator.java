bb4a79b41fa91dd13d41e93b5fa4c31af6a933fd
Andrey Burban <burban.andrey@gmail.com>
2023-07-24 11:28:32 +0300

    HW_7 - implement #7. Fixes of the 2d array check.

--- src/main/java/hw7/ArrayValueCalculator.java
+++ src/main/java/hw7/ArrayValueCalculator.java
@@ -8,14 +8,24 @@ public class ArrayValueCalculator {
 
     public int doCalc(String[][] testArray) throws ArraySizeException, ArrayDataException {
 
-        if (testArray.length != 4) {
+        int testArrayRows = testArray.length;
+        int testArrayCols = testArray[0].length;
+
+        for (int i = 1; i < testArrayRows; i++) {
+            if (testArray[i].length != testArrayCols) {
+                throw new ArraySizeException("Wrong array size! Array must be 4x4", LocalDateTime.now());
+            }
+        }
+
+        if (testArrayRows != 4 || testArrayCols != 4) {
             throw new ArraySizeException("Wrong array size! Array must be 4x4", LocalDateTime.now());
         }
+
         int sum = 0;
         for (int i = 0; i < testArray.length; i++ ) {
             for (int j = 0; j < testArray[i].length; j++) {
                 try{
-                    sum = sum + parseInt(testArray[i][j]);
+                    sum += parseInt(testArray[i][j]);
                 } catch(NumberFormatException e) {
                     throw new ArrayDataException("Wrong data format! [in array #: " + (i+1) + "][in element #: " + (j+1) + "]", testArray[i][j], LocalDateTime.now());
                     }
